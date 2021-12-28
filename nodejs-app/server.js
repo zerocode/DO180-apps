09bc033142c22afc84b23a10043191920815aff0
Student User <student@workstation.lab.example.com>
2021-12-27 22:10:10 -0500

    change path

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -3,7 +3,8 @@ var createError = require('html-errors');
 var express = require('express');
 app = express();
 
-app.get('/hello', function (req, res) {
+app.get('/', function (req, res) {
+	console.log('get hit');
   res.send('Hello World from pod: ' + process.env.HOSTNAME + '\n')
 });
 
